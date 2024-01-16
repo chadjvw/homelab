e758828f909bb66e20a1e3cc268a2e3fac8ee8e1
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2024-01-16 15:15:05 -0700

    Add test nginx container

--- main.ts
+++ main.ts
@@ -1,12 +1,30 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
+import { KubeDeployment } from './imports/k8s';
 
-export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
-    super(scope, id, props);
+class MyChart extends Chart {
+  constructor(scope: Construct, appLabel: string, props: ChartProps = {}) {
+    super(scope, appLabel);
 
-    // define resources here
-
+    // Define a Kubernetes Deployment
+    new KubeDeployment(this, 'my-deployment', {
+      spec: {
+        replicas: 3,
+        selector: { matchLabels: { app: appLabel } },
+        template: {
+          metadata: { labels: { app: appLabel } },
+          spec: {
+            containers: [
+              {
+                name: 'app-container',
+                image: 'nginx:1.19.10',
+                ports: [{ containerPort: 80 }]
+              }
+            ]
+          }
+        }
+      }
+    });
   }
 }
 
