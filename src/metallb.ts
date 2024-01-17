773c21e63a23b275b538c65c5a7f20d09fa95006
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2024-01-17 00:44:57 -0700

    Try adding metallb namespace

--- src/metallb.ts
+++ src/metallb.ts
@@ -1,12 +1,24 @@
 import { Construct } from 'constructs'
 import { Chart } from 'cdk8s'
+import { Namespace } from 'cdk8s-plus-27'
 import { HelmChart } from '../imports/helm.cattle.io'
 
 export class MetalLbChart extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id)
 
-    new HelmChart(this, 'helm-chart', {
+    const namespace = new Namespace(this, 'namespace', {
+      metadata: {
+        labels: {
+          'pod-security.kubernetes.io/audit': 'privileged',
+          'pod-security.kubernetes.io/enforce': 'privileged',
+          'pod-security.kubernetes.io/warn': 'privileged',
+        },
+        name: 'metallb-system',
+      },
+    })
+
+    const metallb = new HelmChart(this, 'helm-chart', {
       metadata: {
         namespace: 'kube-system',
       },
@@ -16,5 +28,7 @@ export class MetalLbChart extends Chart {
         targetNamespace: 'metallb-system',
       },
     })
+
+    metallb.node.addDependency(namespace)
   }
 }
