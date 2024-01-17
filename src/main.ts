bd0a54888f981533971b99143d832819f10b3206
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2024-01-17 08:51:54 -0700

    Add argo chart to cdk8s app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { App } from 'cdk8s'
 import { SmokePingChart } from './services/smoke-ping'
 import { MetalLbChart } from './metallb'
+import { ArgoCDChart } from './argocd'
 
 const app = new App()
 
+new ArgoCDChart(app, 'argocd-app')
 new MetalLbChart(app, 'metallb')
 new SmokePingChart(app, 'smoke-ping')
 
