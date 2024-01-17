8454b99b419efdc86bb83ae1af37718d138f22ae
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2024-01-17 16:18:10 -0700

    Order manifests so helm charts go first

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { App } from 'cdk8s'
 import { SmokePingChart } from './services/smoke-ping'
 import { MetalLbChart } from './metallb'
 import { ArgoCDChart } from './argocd'
+import { HelmCharts } from './helms'
 
 const app = new App()
 
 new ArgoCDChart(app, 'argocd')
-new MetalLbChart(app, 'metallb')
+
+const helms = new HelmCharts(app, 'k3s-helm-charts')
+const metalConfig = new MetalLbChart(app, 'metallb-config')
+metalConfig.addDependency(helms)
+
 new SmokePingChart(app, 'smoke-ping')
 
 app.synth()
--- src/metallb.ts
+++ src/metallb.ts
@@ -8,32 +8,9 @@ export class MetalLbChart extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id)
 
-    const namespace = new Namespace(this, 'namespace', {
-      metadata: {
-        labels: {
-          'pod-security.kubernetes.io/audit': 'privileged',
-          'pod-security.kubernetes.io/enforce': 'privileged',
-          'pod-security.kubernetes.io/warn': 'privileged',
-        },
-        name: 'metallb-system',
-      },
-    })
-
-    new HelmChart(this, 'helm-chart', {
-      metadata: {
-        namespace: 'kube-system',
-      },
-      spec: {
-        repo: 'https://metallb.github.io/metallb',
-        chart: 'metallb',
-        version: '~0.13.12',
-        targetNamespace: 'metallb-system',
-      },
-    })
-
     new IpAddressPool(this, 'ip-pool', {
       metadata: {
-        namespace: namespace.name,
+        namespace: 'metallb-system',
       },
       spec: {
         addresses: ['10.0.2.0-10.0.2.50'],
@@ -43,7 +20,7 @@ export class MetalLbChart extends Chart {
 
     new BgpPeerV1Beta2(this, 'bgp-peer', {
       metadata: {
-        namespace: namespace.name,
+        namespace: 'metallb-system',
       },
       spec: {
         myAsn: 64500,
