c1cb7a0610c73d50f3d7934095ff8608cec73d92
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2024-01-17 01:06:20 -0700

    Add metallb namespace first

--- src/metallb.ts
+++ src/metallb.ts
@@ -1,29 +1,24 @@
 import { Construct } from 'constructs'
 import { Chart } from 'cdk8s'
+import { Namespace } from 'cdk8s-plus-27'
 import { HelmChart } from '../imports/helm.cattle.io'
 
 export class MetalLbChart extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id)
 
-    // const namespace = new Namespace(this, 'namespace', {
-    //   metadata: {
-    //     labels: {
-    //       'pod-security.kubernetes.io/audit': 'privileged',
-    //       'pod-security.kubernetes.io/enforce': 'privileged',
-    //       'pod-security.kubernetes.io/warn': 'privileged',
-    //     },
-    //     name: 'metallb-system',
-    //   },
-    // })
+    const namespace = new Namespace(this, 'namespace', {
+      metadata: {
+        labels: {
+          'pod-security.kubernetes.io/audit': 'privileged',
+          'pod-security.kubernetes.io/enforce': 'privileged',
+          'pod-security.kubernetes.io/warn': 'privileged',
+        },
+        name: 'metallb-system',
+      },
+    })
 
-    // const metallb = new Helm(this, 'redis', {
-    //     chart: 'metallb',
-    //     repo: 'https://metallb.github.io/metallb',
-    //     namespace: ''
-    //   });
-
-    new HelmChart(this, 'helm-chart', {
+    const metallb = new HelmChart(this, 'helm-chart', {
       metadata: {
         namespace: 'kube-system',
       },
@@ -33,5 +28,7 @@ export class MetalLbChart extends Chart {
         targetNamespace: 'metallb-system',
       },
     })
+
+    metallb.node.addDependency(namespace)
   }
 }
